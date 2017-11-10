d14fef45662083dcb16d0c498df13e02e0161153
CurationDockerEngine <felix@cloudburo.net>
2017-11-10 13:30:56 +0000

    PublishingBot Changes: INF.Blog.Github

---
+++ js/app.js
@@ -0,0 +1,1 @@
+(function(){!function(t){return t(window).on("load",function(){return t(document).delegate('*[data-toggle="lightbox"]',"click",function(n){return n.preventDefault(),t(this).ekkoLightbox()})})}(jQuery)}).call(this);
